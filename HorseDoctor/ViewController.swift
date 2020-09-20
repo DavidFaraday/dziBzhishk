7a34d7baf7754e3a1a99af1d84703f47234afb3e
DavidFaraday <iosdevschool@gmail.com>
2020-09-20 08:32:19 +0300

    Initial Commit

---
+++ HorseDoctor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HorseDoctor
+//
+//  Created by David Kababyan on 20/09/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
